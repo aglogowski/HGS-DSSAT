450b60df9d3335f933c79dc2de9779ebbff1aa5e
[paul s] <paul.southard@unibas.ch>
2025-02-10 21:24:03 +0100

    updating groks for mapping needs and coupled model needs

--- src/mapping.py
+++ src/mapping.py
@@ -1,5 +1,6 @@
 
 def CreatePlanviewMapping(hgs_nodes_shp_path,dssat_models_shp_path):
+    import geopandas as gpd
     # Load both shape files
     hgs_gdf = gpd.read_file(hgs_nodes_shp_path)
     dssat_gdf = gpd.read_file(dssat_models_shp_path)
@@ -10,6 +11,8 @@ def CreatePlanviewMapping(hgs_nodes_shp_path,dssat_models_shp_path):
     return hd_dict
 
 def CreateHGSNodesShape(hgs_nodes_xyz_path):
+    import pandas as pd
+    import geopandas as gpd
     # Load xyz file
     df = pd.read_csv(hgs_nodes_xyz_path, header = None, names = ['Node','X','Y','Z'], sep = '\s+')
     # Create point geometry column
@@ -19,6 +22,7 @@ def CreateHGSNodesShape(hgs_nodes_xyz_path):
     gdf.to_file(hgs_nodes_xyz_path[:-4]+'.shp')
 
 def SaveMapping(hd_dict,mapping_pkl_path):
+    import pickle as pkl
     # Pickle mapping dictionary
     with open(mapping_pkl_path,'wb') as file:
         pkl.dump(hd_dict,file)
