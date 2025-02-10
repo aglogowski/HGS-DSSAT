ddc3341403fb5d5ce7ccaabb244634634416bf9b
[paul s] <paul.southard@unibas.ch>
2025-02-10 21:20:15 +0100

    completed mapping file

---
+++ src/mapping.py
@@ -0,0 +1,24 @@
+
+def CreatePlanviewMapping(hgs_nodes_shp_path,dssat_models_shp_path):
+    # Load both shape files
+    hgs_gdf = gpd.read_file(hgs_nodes_shp_path)
+    dssat_gdf = gpd.read_file(dssat_models_shp_path)
+    # Perform spatial join to combine attributes
+    merged = hgs_gdf.sjoin(dssat_gdf, how = 'left', predicate = 'intersects').loc[:,['Node','DSSAT_ID']]
+    ## Create Mapping Dictionary
+    hd_dict = dict(zip(merged['Node'], merged['DSSAT_ID']))
+    return hd_dict
+
+def CreateHGSNodesShape(hgs_nodes_xyz_path):
+    # Load xyz file
+    df = pd.read_csv(hgs_nodes_xyz_path, header = None, names = ['Node','X','Y','Z'], sep = '\s+')
+    # Create point geometry column
+    df['geometry'] = df.apply(lambda x: Point(x['X'],x['Y']), axis = 1)
+    # Export as shapefile
+    gdf = gpd.GeoDataFrame(data = df, geometry = 'geometry')
+    gdf.to_file(hgs_nodes_xyz_path[:-4]+'.shp')
+
+def SaveMapping(hd_dict,mapping_pkl_path):
+    # Pickle mapping dictionary
+    with open(mapping_pkl_path,'wb') as file:
+        pkl.dump(hd_dict,file)
